6cebf1349e8fc74146159fe152a6faf687c2527b
Iztok Jeras <iztok.jeras@gmail.com>
2021-11-15 21:36:13 +0100

    DREVIEW: Chapter 02: added text output to testbench

--- Chapter02/Answers to Exercises/src/Ex__6_adder4_fulltestbench.vhdl
+++ Chapter02/Answers to Exercises/src/Ex__6_adder4_fulltestbench.vhdl
@@ -39,26 +39,29 @@ begin
   begin
 
     -- Test all combinations of two 4-bit addends (256 total tests)
-    for a_val in 0 to 15 loop
-      for b_val in 0 to 15 loop
+    for a_val in 0 to 2**4-1 loop
+      for b_val in 0 to 2**4-1 loop
         -- Set the inputs to the ADDER4 component
         a <= std_logic_vector(to_unsigned(a_val, a'length));
         b <= std_logic_vector(to_unsigned(b_val, b'length));
         wait for 1 ns;
 
         -- Compute the 5-bit sum of the two 4-bit values
-        expected <= unsigned('0' & a) + unsigned('0' & b);
+        expected <= to_unsigned(a_val + b_val, expected'length);
         wait for 1 ns;
 
         -- The 'error' signal will only go to 1 if an error occurs
 	if unsigned(c_out & s) = unsigned(expected) then
           error <= '0';
+	  report "CORRECT:";
         else
           error <= '1';
+	  report "WRONG:";
         end if;
+	report integer'image(a_val) & " + " & integer'image(a_val) & " = " & integer'image(to_integer(expected));
 
         -- Each pass through the inner loop takes 10 ns
-        wait for 7 ns;
+        wait for 8 ns;
 
       end loop;
     end loop;
